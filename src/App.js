15385c9bb5528b02124bdbb98614dde929bdda18
keshav <keshav.mathur@toppr.com>
2021-07-29 17:41:36 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect } from "react";
+import "./App.css";
+
+var newWindow;
 
 function App() {
+  useEffect(() => {
+    const data = {
+      ClientId: "clientId-123",
+      ClientSecret: "clientSecret-123",
+      "User-profile-id": "User-profile-id-123",
+    };
+
+    window.addEventListener(
+      "message",
+      (event) => {
+        console.log(`recieved data from - ${event.origin}`);
+        console.log(event.data);
+        if (event.data === "target loaded") {
+          newWindow.postMessage(data, "https://co-dt-target.herokuapp.com");
+        }
+        if (event.origin === "https://co-dt-target.herokuapp.com") {
+          console.log("EVENT RECIEVED FROM TARGET IN SOURCE");
+          return;
+        }
+        if (event.origin === "https://co-dt-source.web.app") {
+          console.log("EVENT RECIEVED FROM SOURCE IN SOURCE");
+          return;
+        }
+      },
+      false
+    );
+
+    return () => {
+      window.removeEventListener("message");
+    };
+  }, []);
+
+  const clickToSendData = (e) => {
+    /* iframeRef.current.contentWindow.postMessage(
+      data,
+      "https://co-dt-target.herokuapp.com"
+      // "*"
+    ); */
+    newWindow = window.open("https://co-dt-target.herokuapp.com", "_blank");
+
+    /* setTimeout(() => {
+      console.log("sending data to popup window after 3 seconds");
+      newWindow.postMessage(data, "https://co-dt-target.herokuapp.com");
+    }, 3000);
+
+    newWindow.onload = function (e) {
+      console.log(e.origin);
+      newWindow.postMessage(data, "https://co-dt-target.herokuapp.com");
+      newWindow.opener.postMessage(data, "https://co-dt-target.herokuapp.com");
+    }; */
+    /* newWindow.parent.postMessage(data);
+    newWindow.addEventListener("message", (message) => {
+      console.log("Message for newWindow only");
+      console.log(message);
+    }); */
+  };
+
+  const iframeRef = useRef();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ cursor: "pointer" }} onClick={clickToSendData}>
+          Testing Cross Origin Data Transfer - Source
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* <iframe
+          src="https://co-dt-target.herokuapp.com/"
+          title="testing"
+          ref={iframeRef}
+          style={{ border: "none" }}
+          width="600"
+          height="400"
+        ></iframe> */}
+        {/* <object
+          data="https://co-dt-target.herokuapp.com/"
+          width="600"
+          height="400"
         >
-          Learn React
-        </a>
+          <embed
+            src="https://co-dt-target.herokuapp.com/"
+            width="600"
+            height="400"
+          ></embed>
+          Error: Embedded data could not be displayed.
+        </object> */}
+        {/* <embed
+          src="https://co-dt-target.herokuapp.com/"
+          width="600"
+          height="400"
+        ></embed> */}
+        {/* <embed src="https://www.google.com/" width="600" height="400"></embed> */}
       </header>
     </div>
   );
